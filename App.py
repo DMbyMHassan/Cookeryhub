27dc35473881ecb40c9abef997ffee587d6fca8b
Mahmoud Hassan <129586375+DMbyMHassan@users.noreply.github.com>
2023-08-14 03:15:01 +0300

    Adding for App.py

--- App.py
+++ App.py
@@ -1,28 +1,51 @@
-from flask impotr Flask
+from flask import Flask, render_template, request
 import pandas as pd
 import os
 
 app = Flask(__name__)
 
-#configurations
-app.config['UPLOA_FOLDER'] ='static/images'
+# configurations
+app.config['UPLOAD_FOLDER'] = 'static/images'
 
 ''' Load the recipes data from into pandas DataFrame'''
 csv_file = 'recipes.csv'
-df_recipes =pd.read_csv(csv_file)
+df_recipes = pd.read_csv(csv_file)
 
-def hompage():
+
+@app.route('/')
+def homepage():
     return render_template('hmomepage.html')
 
-#test line to check git hub
-def recipes():
-    return render_template('recipes.html')
 
+@app.route('/recipes')
+# test line to check git hub
+def recipes():
+    return render_template('recipes.html', recipes=df_recipes.to_dict(orient='records'))
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def uplod_form():
-    if
+    if request.method == 'POST':
+        recipe_name = request.form['recipe_name']
+        ingredients = request.form['ingredients']
+        preparation_instructions = request.form['preperation_instructions']
+        serving_instructions = request.form['serving_instrucyios']
+        image = request.files['image']
+        """Validation the form data and process the image upload"""
+        if recipe_name and ingredients and preparation_instructions and serving_instructions and image:
+            image.save(os.path.join(app.config['UPLOAD_FOLDER']))
+            new_recipe = {
+                'Recipe_Name': recipe_name,
+                'Preparation_Instructions': preparation_instructions,
+                'Serving_Instructions' :serving_instructions,
+                'Image_URL': f'static/images/{image.filename}'
+            }
+            df_recipes.append(new_recipe,ignore_index =True)
+            df_recipes.to_csv(csv_file,index=False)
         return
     else:
         return
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
